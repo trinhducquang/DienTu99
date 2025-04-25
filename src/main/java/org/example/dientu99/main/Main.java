4342d2c8bafc3a786e57adc8bc6c31fb610802ef
ducluonghust <141119101+ducluonghust@users.noreply.github.com>
2025-04-26 06:49:39 +0700

    Update Main.java

--- src/main/java/org/example/dientu99/main/Main.java
+++ src/main/java/org/example/dientu99/main/Main.java
@@ -11,7 +11,7 @@ import java.sql.Connection;
 import java.util.Objects;
 
 
-public class Main extends Application {
+public class    Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/org/example/dientu99/views/login/Login.fxml")));
